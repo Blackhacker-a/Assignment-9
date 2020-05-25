7ec373cb581525218ca5f7a70f10a14e8b27ae17
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-25 09:17:11 +0700

    em nop bai 2

---
+++ Bai 2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+void Nhapchuoi(char str[100]){
+	scanf("%s",str);
+}
+
+//void Xuat chuoi
+
+bool check(char str1[100], char str2[100]){
+    if(strstr(str1,str2)!=NULL){
+    	return true;
+	}
+}
+
+int main(){
+    char str1[100];
+    printf("Nhap vao chuoi 1 ");
+	Nhapchuoi(str1);
+	char str2[100];
+	printf("Nhap vao chuoi 2 ");
+	Nhapchuoi(str2);	
+   	bool c=check(str1,str2);
+   	if(c==false){
+   		printf("Chuoi 1 khong chua chuoi 2");
+	   } else{
+	   	printf("Chuoi 1 co chua chuoi 2 ");
+	   }
+}
